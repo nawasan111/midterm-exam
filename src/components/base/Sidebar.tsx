324c8ab02ef59984e2e43716a8590d1dccafe5bf
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2023-08-19 17:45:12 +0700

    why ? why ? why ????

--- src/components/base/Sidebar.tsx
+++ src/components/base/Sidebar.tsx
@@ -12,7 +12,7 @@ export default function Sidebar() {
       tabIndex={-1}
       id="offcanvasWithBothOptions"
       aria-labelledby="offcanvasWithBothOptionsLabel"
-      data-bs-toggle="offcanvas"
+      // data-bs-toggle="offcanvas"
       data-bs-target="#offcanvasWithBothOptions"
       aria-controls="offcanvasWithBothOptions"
       style={{ width: 230 }}
